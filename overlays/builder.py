1c4168cf0dcda972b4f352eca3dea91878ab51e8
Robert Wolterman <robert.wolterman@gmail.com>
2016-07-15 23:32:44 -0500

    Start of the overlay feature, grabbed the sample dts from CHIP-dt-overlay, modified builder.py from the BeagleBone Black repo

---
+++ overlays/builder.py
@@ -0,0 +1,17 @@
+from subprocess import call
+import os
+import shutil
+import glob
+
+def compile():
+    call(["dtc", "-O", "dtb", "-o", "overlays/chip-spi2.dtbo", "-b", "o", "-@", "overlays/chip-spi2.dts"])
+    call(["dtc", "-O", "dtb", "-o", "overlays/chip-i2c1.dtbo", "-b", "o", "-@", "overlays/chip-i2c1.dts"])
+    call(["dtc", "-O", "dtb", "-o", "overlays/chip-pwm0.dtbo", "-b", "o", "-@", "overlays/chip-pwm0.dts"])
+
+def copy():
+    for fl in glob.glob("/lib/firmware/chip_io/chip-*-.dtbo"):
+        os.remove(fl)
+    shutil.move("overlays/chip-spi2.dtbo", "/lib/firmware/chip_io/chip-spi2.dtbo")
+    shutil.move("overlays/chip-i2c1.dtbo", "/lib/firmware/chip_io/chip-i2c1.dtbo")
+    shutil.move("overlays/chip-pwm0.dtbo", "/lib/firmware/chip_io/chip-pwm0.dtbo")
+
